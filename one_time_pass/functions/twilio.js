894b02e2c2f757fa50ac950656f01b49d914aad5
Anushree-Patil <61741834+Anushree-Patil@users.noreply.github.com>
2020-08-18 21:56:51 +0530

    Update twilio.js

--- one_time_pass/functions/twilio.js
+++ one_time_pass/functions/twilio.js
@@ -1,8 +1,8 @@
 const twilio =require('twilio')
 
 //two tokens that will allow us to send message or authenticate us with the twilio API
-const accountSid='AC5d862bb40f2aad0d843f331d0eb20602'
-const authToken ='ace9385875edb4f155be34f5f5f7538c'
+const accountSid=''
+const authToken =''
 
 
 //create twilio client and export it from this file
